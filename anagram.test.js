99fb9704aafa8c992fe357ff23faf9bf005613c0
Jordan Blakey <454820+jordanblakey@users.noreply.github.com>
2024-07-13 21:08:15 -0500

    beforeEach/All, afterEach/All, describe

--- anagram.test.js
+++ anagram.test.js
@@ -1,17 +1,52 @@
 const isAnagram = require("./anagram");
 
-test("isAnagram function exists", () => {
-  expect(typeof isAnagram).toEqual("function");
+const initDatabase = () => console.log("Database initialized...");
+const closeDatabase = () => console.log("Database closed...");
+
+beforeAll(() => {
+  console.log("This runs just once beforeAll tests");
+  initDatabase();
 });
 
-test('"cinema" is an anagram of "iceman"', () => {
-  expect(isAnagram("cinema", "iceman")).toBeTruthy();
+afterAll(() => {
+  console.log("This runs just once afterAll tests");
+  closeDatabase();
 });
 
-test('"Dormitory" is an anagram of "dirty room##"', () => {
-  expect(isAnagram("Dormitory", "dirty room##")).toBeTruthy();
+const nameCheck = () => console.log("Checking Name...");
+
+describe("Checking Names", () => {
+  beforeEach(() => {
+    nameCheck();
+  });
+
+  test("User is Jeff", () => {
+    const user = "Jeff";
+    expect(user).toBe("Jeff");
+  });
+
+  test("User is Karen", () => {
+    const user = "Karen";
+    expect(user).toBe("Karen");
+  });
+
+  afterEach(() => {});
 });
 
-test('"Hello" is NOT an anagram of "Aloha"', () => {
-  expect(isAnagram("Hello", "Aloha")).toBeFalsy();
+describe("isAnagram Tests", () => {
+  test("isAnagram function exists", () => {
+    expect(typeof isAnagram).toEqual("function");
+  });
+
+  test('"cinema" is an anagram of "iceman"', () => {
+    expect(isAnagram("cinema", "iceman")).toBeTruthy();
+  });
+
+  test('"Dormitory" is an anagram of "dirty room##"', () => {
+    expect(isAnagram("Dormitory", "dirty room##")).toBeTruthy();
+  });
+
+  test('"Hello" is NOT an anagram of "Aloha"', () => {
+    expect(isAnagram("Hello", "Aloha")).toBeFalsy();
+  });
 });
